a6d0256db9fe6b2ae661c9470bbc4db00fcf7ec1
Gene Dan <genedan@gmail.com>
2023-08-23 20:01:58 -0500

    FIX: License info.

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3.10",
-        "License :: OSI Approved :: Mozilla Public License Version 2.0",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
         "Operating System :: POSIX :: Linux",
     ],
     python_requires='>=3.10.0',
